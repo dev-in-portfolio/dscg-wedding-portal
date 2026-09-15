f13ce4135dcfae0164acd6c90f23c9244895ca7c
dev-in-portfolio <devin.dev.portfolio@gmail.com>
2026-09-14 21:33:42 -0400

    Route wedding portal intake buttons

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -18,7 +18,7 @@ export const CONFIG = {
   WEEKEND_DESTINATION_DEMO_URL: 'https://wedding-weekend-destination-demo.netlify.app',
 
   // Site 5: DSCG Wedding Intake Application
-  WEDDING_INTAKE_URL: '',
+  WEDDING_INTAKE_URL: 'https://websites-intake.darkstarconsultinggroup.com/intake',
 
   // Metadata
   COMPANY_NAME: 'Dark Star Consulting Group',
